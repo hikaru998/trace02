9d24fa0d9282247b3d9ea06b8ff9ba9522c9fbdc
hikaru998 <hika9894.s12@ezweb.ne.jp>
2019-04-19 21:01:56 +0900

    four

--- sango.js
+++ sango.js
@@ -1,3 +1,16 @@
 $(function(){
-    
+    $('.tab-btn').click(function(){
+        $('.article-wrapper').removeClass('is-active-item');
+        $($(this).attr('href')).addClass('is-active-item');
+    });
+    var $newArticle = $('.new-article');
+    var $recommendedArticle = $('.recommended-article');
+    $newArticle.click(function(){
+        $newArticle.addClass('is-active-btn');
+        $recommendedArticle.removeClass('is-active-btn');
+    });
+    $recommendedArticle.click(function(){
+        $recommendedArticle.addClass('is-active-btn');
+        $newArticle.removeClass('is-active-btn');
+    })
 });
